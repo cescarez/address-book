e53f3e839f536bb3bc49923a7119c09d46416341
Katy and Christabel Escarez <christabel@>
2018-10-10 20:04:03 -0700

    Add ability to add multiple addresses per contact

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@ function Contact(first, last, favColor, favAnimal, address) {
   this.lastName = last;
   this.favColor = favColor;
   this.favAnimal = favAnimal;
-  this.address = []
+  this.addresses = []
 };
-function Address(street, city, state){
+
+function Address(street, city, state) {
   this.street = street;
   this.city = city;
   this.state = state;
 };
+
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
@@ -18,6 +20,23 @@ Address.prototype.fullAddress = function() {
 }
 
 $(document).ready(function() {
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+      '<div class="form-group">' +
+      '<label for="new-street">Street</label>' +
+      '<input type="text" class="form-control new-street">' +
+      '</div>' +
+      '<div class="form-group">' +
+      '<label for="new-city">City</label>' +
+      '<input type="text" class="form-control new-city">' +
+      '</div>' +
+      '<div class="form-group">' +
+      '<label for="new-state">State</label>' +
+      '<input type="text" class="form-control new-state">' +
+      '</div>' +
+      '</div>')
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -28,18 +47,37 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedFavColor, inputtedFavAnimal);
 
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
     $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-    $(".favorite-color").text(newContact.favColor);
-    $(".favorite-animal").text(newContact.favAnimal);
-  });
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".favorite-color").text(newContact.favColor);
+      $(".favorite-animal").text(newContact.favAnimal);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+      });
+
+    });
+
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-favorite-color").val("");
     $("input#new-favorite-animal").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+
   });
 });
